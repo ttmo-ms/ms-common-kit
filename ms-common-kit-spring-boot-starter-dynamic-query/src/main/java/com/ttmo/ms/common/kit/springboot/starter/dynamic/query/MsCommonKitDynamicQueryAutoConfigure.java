e2f5253fad934b921c4b48cb0e51129347b946be
JoverZhang <joverzh@gmail.com>
2020-07-30 11:52:52 +0800

    feat: ms-common-kit-spring-boot-starter-dynamic-query.

---
+++ ms-common-kit-spring-boot-starter-dynamic-query/src/main/java/com/ttmo/ms/common/kit/springboot/starter/dynamic/query/MsCommonKitDynamicQueryAutoConfigure.java
@@ -0,0 +1,26 @@
+package com.ttmo.ms.common.kit.springboot.starter.dynamic.query;
+
+
+import com.ttmo.ms.common.kit.dynamic.query.aspect.DynamicQueryAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TODO
+ *
+ * @author Jover Zhang
+ */
+@Slf4j
+@Configuration
+public class MsCommonKitDynamicQueryAutoConfigure {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DynamicQueryAspect dynamicQueryAspect() {
+        log.info("Init ms-common-kit-dynamic-query");
+        return new DynamicQueryAspect();
+    }
+
+}
